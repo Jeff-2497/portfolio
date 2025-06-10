ae0e25dceb0cd693f8721722a94fc571d9f7ba36
Jeffrey S. <spangj49@gmail.com>
2025-06-10 15:00:03 -0400

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,45 +1,89 @@
+// on the intial page, this is more the arrow animation when hovered
 const arrowImg = document.getElementById('arrow-img');
 const bottom = document.querySelector('.bottom');
 
-arrowImg.addEventListener('mouseover', () => {
-    bottom.style.marginBottom = '45px';
-});
-
-arrowImg.addEventListener('mouseout', () => {
+if(arrowImg) {
+  arrowImg.addEventListener('mouseover', () => {
     bottom.style.marginBottom = '30px';
-});
+  });
 
-
-window.onscroll = onScroll;
-
-function onScroll() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.body.classList.remove("scroll-top");
-  } else {
-    document.body.classList.add("scroll-top");
-  }
+  arrowImg.addEventListener('mouseout', () => {
+    bottom.style.marginBottom = '10px';
+  });
 }
 
 // "to top" button
-
 function upFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
 //fade in and fade out on scroll
-
 // Set a function onscroll - this will activate if the user scrolls
 window.onscroll = function() {
-  // Set the height to check for
   let appear = 200
+
   if (window.scrollY >= appear) {
-    // If more show the element
     document.getElementById("buttonUp").style.opacity = '1'
     document.getElementById("buttonUp").style.pointerEvents = 'all'
   } else {
-    // Else hide it
     document.getElementById("buttonUp").style.opacity = '0'
     document.getElementById("buttonUp").style.pointerEvents = 'none'
   }
 }
+
+// navbar fade in and out when scroll gets to #main-target i.e. first section
+const nav = document.getElementById("nav");
+const mainTarget = document.getElementById("main-target");
+
+if (mainTarget) {
+  window.addEventListener("scroll", () => {
+    const targetPosition = mainTarget.getBoundingClientRect().top;
+
+    if (targetPosition <= 0) {
+      nav.classList.remove("hidden");
+    } else {
+      nav.classList.add("hidden");
+    }
+  }); 
+}
+
+
+// circle following cursor
+const circle = document.querySelector('.cursor-circle');
+let mouseX = 0;
+let mouseY = 0;
+let circleX = 0;
+let circleY = 0;
+const speed = 0.2; 
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+function animate() {
+  circleX += (mouseX - circleX) * speed;
+  circleY += (mouseY - circleY) * speed;
+  circle.style.left = `${circleX}px`;
+  circle.style.top = `${circleY}px`;
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+// when scrolling up navabr appears otherwise its invisible
+let lastScrollY = window.scrollY;
+const nav2 = document.querySelector("nav");
+let threshold = 10;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  if (!nav2) return;
+
+  if (currentScroll - lastScrollY > threshold) {
+    nav2.classList.add("hidden");
+  } else if (lastScrollY - currentScroll > threshold) {
+    nav2.classList.remove("hidden");
+  }
+
+  lastScrollY = currentScroll;
+});
